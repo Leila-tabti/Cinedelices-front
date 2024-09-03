eecbcad114b59b31e91248e60b1d1344bb2c5f89
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-03 16:10:43 +0200

    LL-feat_RecipeDetails

--- CineDelice-front/src/pages/RecipeDetails/RecipeDetails.tsx
+++ CineDelice-front/src/pages/RecipeDetails/RecipeDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ratatouille from '../../assets/Pictures/Recipes/ratatouille.jpg';
 import ratatouilleMovie from '../../assets/Pictures/MoviesSeries/ratatouille.png';
 import './RecipeDetails.scss';
+import { RiH3 } from 'react-icons/ri';
 
 export default function RecipeDetails () {
     return(
@@ -10,16 +11,16 @@ export default function RecipeDetails () {
             <p className='description'>
             Le plat principal inspiré du film, qui est la quintessence de la cuisine provençale avec des légumes frais et savoureux.
             </p>
-            <img src={ratatouille} alt="image ratatouille" />
-            <p className='inspiredBy'>Inspiré du film :</p>
-            <h2>Ratatouille</h2>
-            <img src={ratatouilleMovie} alt="image ratatouille le film" />
-            <div className='otherRecipesContainer'>
-                <p className='otherRecipes'>
-                    Voir d'autres recettes liées à Ratatouille 
-                </p>
-                <img src="{ratatouille}" alt="" />
-                <img src="{ratatouille}" alt="" />
+            <div className='second-container'>
+                <img src={ratatouille} alt="image ratatouille" />
+                <p className='inspiredBy'>Inspiré du film :</p>
+                <h2>Ratatouille</h2>
+                <img src={ratatouilleMovie} alt="image ratatouille le film" />
+                <div className='otherRecipesContainer'>
+                    <h3 className='otherRecipes'>
+                        Voir d'autres recettes liées à Ratatouille 
+                    </h3>
+            </div>
             </div>
             <div className='ingredientsContainer'>
                 <h3>Liste des ingrédients</h3>
@@ -35,17 +36,20 @@ export default function RecipeDetails () {
                     <li>1 cuillère à café d'herbes de Provence</li>
                     <li>Sel et poivre</li>
                 </ul>
-                <h3>instructions</h3>
-                <p>
-                Faites fondre le beurre avec l'huile d'olive dans une grande casserole à feu moyen. Ajoutez les oignons émincés et faites-les revenir doucement jusqu'à ce qu'ils soient caramélisés et dorés (environ 20-25 minutes). <br />
-                Déglacez avec le vin blanc (si utilisé) et laissez réduire pendant 2 minutes. <br />
-                Ajoutez le bouillon de bœuf et le bouquet garni. Laissez mijoter 15-20 minutes. Assaisonnez avec du sel et du poivre. <br />
-                Préchauffez votre four à 200°C (400°F). Disposez les tranches de pain sur une plaque de cuisson et faites-les légèrement griller. <br />
-                Répartissez la soupe dans des bols allant au four, déposez une tranche de pain grillé sur chaque bol, puis recouvrez généreusement de fromage râpé. <br />
-                Faites gratiner sous le gril du four jusqu'à ce que le fromage soit doré et bouillonnant.
+                </div>
+                <div className='second-container'>
+                    <h3>instructions</h3>
+                    <p>
+                    Faites fondre le beurre avec l'huile d'olive dans une grande casserole à feu moyen. Ajoutez les oignons émincés et faites-les revenir doucement jusqu'à ce qu'ils soient caramélisés et dorés (environ 20-25 minutes). <br />
+                    Déglacez avec le vin blanc (si utilisé) et laissez réduire pendant 2 minutes. <br />
+                    Ajoutez le bouillon de bœuf et le bouquet garni. Laissez mijoter 15-20 minutes. Assaisonnez avec du sel et du poivre. <br />
+                    Préchauffez votre four à 200°C (400°F). Disposez les tranches de pain sur une plaque de cuisson et faites-les légèrement griller. <br />
+                    Répartissez la soupe dans des bols allant au four, déposez une tranche de pain grillé sur chaque bol, puis recouvrez généreusement de fromage râpé. <br />
+                    Faites gratiner sous le gril du four jusqu'à ce que le fromage soit doré et bouillonnant.
 
-                </p>         
-            </div>
+                    </p>   
+                </div>      
+            
         </div>
     )
 }

7c50b2848f4189214bfd82c8ec32eaa9f5acbf9a
alexandrapasco <sachapasco@gmail.com>
2024-09-03 17:23:18 +0200

    AP-HomePage-Slides

--- CineDelice-front/src/pages/HomePage/HomePage.tsx
+++ CineDelice-front/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './HomePage.scss';
+import Photo1 from '../../assets/Pictures/Recipes/Photo1.png';
 
-export default function HomePage () {
+export default function HomePage() {
     return (
-        <h1>Bienvenue sur la page d'accueil !</h1>
-    )
+        <main className="homepage">
+            <section className="landing_page">
+                <div className="image_container">
+                    <img src={Photo1} alt="Image de la recette de soupe de potiron de chocolat noir" />
+                </div>
+                <div className="slogan">
+                    <h1>Le goût du cinéma à chaque bouchée... Faites mijoter vos films préférés.</h1>
+                    <h2>Plongez dans un festin cinématographique, où chaque film et série est une invitation à savourer des délices inspirés de l'écran.</h2>
+                    <button className="subscribe_button"><a href="">S'inscrire</a></button>
+                </div>
+            </section>
+        </main>
+    );
 }

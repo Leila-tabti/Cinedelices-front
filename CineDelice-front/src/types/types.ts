c082d598f72fc553346280ba84266b64d297de50
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-05 10:49:52 +0200

    LL-feature/types

---
+++ CineDelice-front/src/types/types.ts
@@ -0,0 +1,49 @@
+export interface IMovieCategory {
+    name: string
+}
+
+export interface IUser {
+    id: number;
+    email: string;
+    pseudo: string;
+    password: string;
+    avatar: string;
+    role: string;
+}
+
+export interface IMovieSerie {
+    id: number;
+    name: string;
+    synopsis: string;
+    director: string;
+    actors: string;
+    releaseDate: string;
+    picture: string;
+    category: IMovieCategory;
+}
+
+export interface IRecipeCategory {
+    id: number;
+    name: string;
+}
+
+export interface IRecipe {
+    id: number;
+    name: string;
+    description: string;
+    picture: string;
+    difficulty: string;
+    time: string;
+    instruction: string;
+    category: IRecipeCategory;
+    movieSerie: IMovieSerie;
+    user: IUser;
+}
+
+export interface IIngredient {
+    id: number;
+    name: string;
+    picture: string;
+}
+
+

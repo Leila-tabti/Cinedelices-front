7be5fa8b18c1f8bed1c16a24e30110f146d447a7
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-10 13:52:47 +0200

    LL - done except for pictures

--- CineDelice-front/src/pages/MoviesSeries/MoviesSeries.tsx
+++ CineDelice-front/src/pages/MoviesSeries/MoviesSeries.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import seigneur from '../../assets/Pictures/MoviesSeries/seigneur.png';
 import { useRootContext } from '../../routes/Root';
 import { IMovieSerie } from '../../types/types';
-
+import { NavLink } from 'react-router-dom';
 import './MoviesSeries.scss';
 
 interface MovieSerieProps {
@@ -17,10 +17,11 @@ export default function MoviesSeries () {
             <h1>Films & Séries</h1>
             <div className="cards-container">
                 {moviesSeries.map((movieSerie) => (
-                   <div key={movieSerie.id} className="card">
-                    
+                    <div key={movieSerie.id} className="card">
+                    <NavLink to={`/MoviesSeries/${movieSerie.id}`}>
                     <img src={seigneur} alt="image_seigneur des anneaux" />
                     <h3>{movieSerie.name}</h3>
+                    </NavLink>
                 </div> 
                 ))}
                 

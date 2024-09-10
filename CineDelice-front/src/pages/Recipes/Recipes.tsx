12464c0dda3ca2798243df4b0e14faa5d4e37662
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-10 16:19:10 +0200

    LL - select

--- CineDelice-front/src/pages/Recipes/Recipes.tsx
+++ CineDelice-front/src/pages/Recipes/Recipes.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IRecipe } from '../../types/types';
 import chiliHeinsenberg  from '../../assets/Pictures/Recipes/chiliHeinsenberg.png';
 import { useRootContext } from '../../routes/Root';
 import './Recipes.scss';
 import { NavLink } from 'react-router-dom';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 
+const optionCategory = [
+    { value: 'entrée', label: 'Entrée'},
+    { value: 'plat', label: 'Plat'},
+    { value: 'dessert', label: 'Dessert'},
+]
 
 
 export default function Recipes () {
+
+    function customTheme(theme){
+        return {
+            ...theme,
+            colors: {
+                ...theme.colors,
+                primary25: 'red',
+                primary: 'red',
+            }
+        }
+    }
     
    const { recipes } = useRootContext();
 
     return (
         <div className="container">
             <h1>Recettes</h1>
+            <Select 
+            theme={customTheme}
+            options={optionCategory}
+            className='select'
+            placeholder="Selectionner une catégorie"
+            />
             <div className="cards-container">
                 {recipes.map((recipe) => (
                 <div key={recipe.id} className="card">

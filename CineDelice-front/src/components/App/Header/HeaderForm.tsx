2fce3f1d1567dd0e2d3febd2a20f28e70ac5f895
Leila-tabti <leila.tabti@oclock.school>
2024-09-16 16:31:02 +0200

    merge

--- CineDelice-front/src/components/App/Header/HeaderForm.tsx
+++ CineDelice-front/src/components/App/Header/HeaderForm.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.scss';
 import { FaSearch } from 'react-icons/fa';
+import Select from 'react-select';
 
-export default function HeaderForm() { 
+const HeaderForm: React.FC = () => {
+
     return (
-        <>
-            
-                <form className="header-form">
-                    <input
-                        type="search"
-                        placeholder="Rechercher..."
-                        aria-label="Recherche CinéDélices"
-                        className="search-input"
-                    />
-                    <button className="btn-search" type="submit" aria-label="Rechercher">
-                        <FaSearch className="icon-search" />
-                    </button>
-                </form>
-            
-        </>
+        <div>
+            <form className="header-form">
+                <input
+                    type="text"
+                    value=""
+                    onChange=""
+                    placeholder="Rechercher..."
+                    aria-label="Recherche CinéDélices"
+                    className="search-input"
+                />
+            </form>
+            <ul>
+                {results.map((item, index) => (
+                    <li key={index}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
     );
-}
+};
+
+export default HeaderForm;

4e1fd4892480a97e6e49e9e77df5df1383a08086
LucasLagassie <lucas.lagassie@oclock.school>
2024-09-17 14:01:26 +0200

    LL - menuBurger background, navbar close

--- CineDelice-front/src/components/App/Navbar/Navbar.tsx
+++ CineDelice-front/src/components/App/Navbar/Navbar.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import './Navbar.scss';
 import { NavLink } from 'react-router-dom';
 
-export default function Navbar() {
+interface NavbarProps {
+  onNavItemClick: () => void;
+}
+
+export default function Navbar({ onNavItemClick }: NavbarProps) {
     return (
       <nav className="navbar">
         <ul className="navbar-list">
-          <li className="navbar-item"><NavLink to='/'>Accueil</NavLink></li>
-          <li className="navbar-item"><NavLink to='/MoviesSeries'>Films & Séries</NavLink></li>
-          <li className="navbar-item"><NavLink to ='/Recipes'>Recettes</NavLink></li>
-          <li className="navbar-item "><NavLink to = '/Login'>Connexion</NavLink></li>
-          <li className="navbar-item "><NavLink to = '/Register'>Inscription</NavLink></li>
+          <li className="navbar-item" onClick={onNavItemClick}><NavLink to='/'>Accueil</NavLink></li>
+          <li className="navbar-item" onClick={onNavItemClick}><NavLink to='/MoviesSeries'>Films & Séries</NavLink></li>
+          <li className="navbar-item" onClick={onNavItemClick}><NavLink to ='/Recipes'>Recettes</NavLink></li>
+          <li className="navbar-item" onClick={onNavItemClick}><NavLink to = '/Login'>Connexion</NavLink></li>
+          <li className="navbar-item" onClick={onNavItemClick}><NavLink to = '/Register'>Inscription</NavLink></li>
         </ul>
       </nav>
     );
